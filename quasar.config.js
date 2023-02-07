1891dfc13d44ae5147df80d3308c2721f7c00bb8
Olga Luchinkina <luchik.web@gmail.com>
2023-02-07 15:57:04 +0200

    dev serve config

--- quasar.config.js
+++ quasar.config.js
@@ -92,7 +92,9 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#devServer
     devServer: {
-      // https: true
+        https: false,
+        host: '127.0.0.1',
+        port: 9001,
       open: true // opens browser window automatically
     },
 
