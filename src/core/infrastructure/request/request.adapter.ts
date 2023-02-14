97cece63937b602e04681d9ef478e9c9729a29cb
Olga Luchinkina <luchik.web@gmail.com>
2023-02-14 18:13:30 +0200

    init

--- src/core/infrastructure/request/request.adapter.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Base Request adapter
- */
-import RequestAdapterAbstract from '@comfy/core/src/infrastructure/request/request.adapter.abstract';
-
-class RequestAdapter extends RequestAdapterAbstract { };
-
-export default new RequestAdapter()
